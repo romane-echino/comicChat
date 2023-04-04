f1944f2c6446e9c0084c5d41b638374cc65f0c72
Romane Donnet <romane@echino.com>
2023-04-04 07:28:00 +0200

    api changes

--- api/index.js
+++ api/index.js
@@ -10,20 +10,7 @@ connectedClients = [];
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 console.log('Salut', uuidv4())
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-});
-
-
-app.post('/disconnect', (req, res) => {
+app.post('/api/disconnect', (req, res) => {
   console.log('/disconnect ---------------')
   console.log('data', JSON.stringify(req.body));
   console.log('---------------')
@@ -47,7 +34,7 @@ app.post('/disconnect', (req, res) => {
 })
 
 
-app.post('/connect', (req, res) => {
+app.post('/api/connect', (req, res) => {
   console.log('/connect ---------------')
   console.log('data', JSON.stringify(req.body));
 
@@ -98,16 +85,14 @@ console.log('isHost (emptyclients)', isHost);
   res.send(response);
 })
 
-app.get('/getclients', (req, res) => {
+app.get('/api/getclients', (req, res) => {
   res.send({ connected: connectedClients });
 });
 
 
-app.get('/resetclients', (req, res) => {
+app.get('/api/resetclients', (req, res) => {
   connectedClients = [];
   res.send({ connected: connectedClients });
 });
-
-
 
 module.exports = app;
